8bfeeb6533e17135072fdc02523b2315c845c64f
4BFC <4bee.code@gmail.com>
2023-10-23 23:32:33 +0900

    eidited button.php& data.js process_data.php new create ajax dir

--- ver_0/button_data/button.php
+++ ver_0/button_data/button.php
@@ -22,9 +22,9 @@ $conn = mysqli_connect(
   <h1>Click BUTTON</h1>
   <form action="process_data.php" method="POST">
     <!-- js에서 해당 언어를 찾고 값을 정의하고 process_data.php 로 값을 전달한다.-->
-    <input type="hidden" name="hidden_text" class="hidden_text" placeholder="input text">
-
-    <input type="button" class="newbtn" value="new value">
+    <!-- hidden type의 value는 값이 없다. 여기서 우리는 먼저 값을 부여한 뒤에 클릭이 이루어져야 한다. -->
+    <input type="hidden" name="hidden_text" class="hidden_text">
+    <input type="hidden" name="hidden_num" class="hidden_num">
     <input type="submit" class="btn" value="Click!">
   </form>
 
--- ver_0/button_data/data.js
+++ ver_0/button_data/data.js
@@ -1,13 +1,18 @@
-let button = document.querySelector(".btn");
 let hidden_text = document.querySelector(".hidden_text");//hidden_text는 현재 type이 숨겨져 있기 때문에 보이지 않는다.
-let new_Btn = document.querySelector(".newbtn"); //btn클릭시 hidden_text의 value 값이 변동
+let hidden_num = document.querySelector(".hidden_num");
+let btn = document.querySelector(".btn");
 
-new_Btn.addEventListener("click", () => {
-  console.log("Before : " + hidden_text.value);
-  hidden_text.value = "test"
+let random = Math.floor(Math.random() * (10 - 0 + 1) + 0);
+
+let addBtn = () => btn.addEventListener("click", () => {
+  hidden_text.value = "test";
+  hidden_num.value = random;
   console.log("After : " + hidden_text.value);
 })
-
+//fetch가 없으면 실행이 안된다.
+fetch('button.php').then(addBtn());
+console.log("Before : " + hidden_text.value);
+console.log(random);
 
 //페이지가 전환이 되면서 해당 button의 콜백함수가 실행이 되지 않는다. 즉, 이전 페이지에서 수행을 하고 바뀐 페이지에서는 해당 log가 찍히지 않는 다는 것을 알 수 있다. 여기서 우리는 POST와 같은  data전송을 해줘야한다. 이로 ajax를 이용해야 한다.
 // button.addEventListener("submit", () => {
